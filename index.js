b347409d8477eb58759285d501735def4007712f
Thomas Bastos <thomas.felip16@gmail.com>
2025-06-06 23:37:31 -0300

    learn: use FS module for create folder

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const {Person} = require("./person");
 
 const person = new Person("John");
 
-require('./modules/path')
+// require('./modules/path')
+// require('./modules/fs')
 
 console.log(person.SayMyName());
 
