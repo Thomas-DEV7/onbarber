fdfaf37777a13ddbe15331230e491982f99097f7
Thomas Bastos <thomas.felip16@gmail.com>
2025-06-07 23:13:21 -0300

    feat: create route for register user

--- modules/express.js
+++ modules/express.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const UserModel = require('../src/models/user.model');
+
 const app = express();
-
-app.get('/home', (req, res) => {
-    res.contentType('application/html');
-    res.status(200).send("<h1>teste</h1>");
-});
-
-app.get('/users', (req, res) => {
-    res.contentType('application/json');
-    const users = [
-        { id: 1, name: 'João', age: 25 },
-        { id: 2, name: 'Maria', age: 30 },
-    ]
-    res.status(200).send(users);
-})
-
 const port = 3000;
 
-app.listen(port, () => console.log(`Rodando com express || http://localhost:${port}`));
+// Middleware para habilitar JSON no body
+app.use(express.json());
+
+// Rota para criação de usuário
+app.post('/users', async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+
+
+
+// Inicialização do servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando: http://localhost:${port}`);
+});
