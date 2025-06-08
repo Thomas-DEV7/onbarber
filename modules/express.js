62b051558b21fc028a8c7147fd9446e52fa25cc5
Thomas Bastos <thomas.felip16@gmail.com>
2025-06-07 23:20:08 -0300

    feat: create route for get user id

--- modules/express.js
+++ modules/express.js
@@ -27,6 +27,21 @@ app.get('/users', async (req, res) => {
     }
 });
 
+// buscar usuario por id
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        if (!user) {
+            res.status(404).send({ message: 'User not found' });
+        } else {
+            res.status(200).send(user);
+
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
 
 // Inicialização do servidor
 app.listen(port, () => {
